67f13b9a859ad88fc979f377d4545ab8a8ee0a98
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-08-25 14:29:02 +0530

    added build

---
+++ build/precache-manifest.024e7c45b2214fc9c5648487f9a33c47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "248291bb3795d240236d0db4446b5879",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dc8462c6f2fb1319d7e",
+    "url": "/static/css/main.009eb937.chunk.css"
+  },
+  {
+    "revision": "7d9a2529d99160b9d1d8",
+    "url": "/static/js/2.737dc4ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.737dc4ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc8462c6f2fb1319d7e",
+    "url": "/static/js/main.a4231bbc.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
